ea867b4d4c4cca3b1bdeee53595070ce5df9ca29
shedrack wambua <shedrackn240@gmail.com>
2023-06-21 12:19:12 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
@@ -8,9 +7,7 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-
 	i = 0;
-
 	while (i < 10)
 	{
 		ch = 'a';
@@ -23,23 +20,3 @@ void print_alphabet_x10(void)
 		i++;
 	}
 }
-
-
-3-islower.c
-
-#include "main.h"
-
-/**
- * _islower - check if char is lowercase
- * @c: is the char to be checked
- * Return: 1 if char is lowercase, otherwise 0.
- */
-
-int _islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
-
